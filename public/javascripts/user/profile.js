563a70a0c514dc558238ba1d813f56a54923a9ca
Frank von Eitzen <fveitzen@gmail.com>
2018-12-25 14:48:30 +0100

    Change own user Profile

--- public/javascripts/user/profile.js
+++ public/javascripts/user/profile.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
 
-    
+    var form = $('form');
+    form.submit( function(event) {
+
+        var form = $(this);
+        var params = form.serializeArray();
+        var userId = $('#_id').val();
+
+        event.preventDefault();
+        var form = $(this);
+        $.ajax({
+            method: "PUT",
+            url: "/api/users/" + userId,
+            data: {
+                "email": $('#email').val(),
+                "realName": $('#realName').val()
+            },
+        })
+        .done(function(data) {
+            alert("Changes for " + $('#username').text() + " have been saved.");
+        })
+        .fail(function() {
+            alert("error");
+        });
+
+    });
+
+    $('#cancel').click(function() {
+        window.location.href = "/user/"
+    })
+
 })
