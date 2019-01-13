e3ea9ef39c4ab68db68d08e796fdb79d69fde666
Frank von Eitzen <fveitzen@gmail.com>
2019-01-13 12:56:54 +0100

    fix test case (block scoped variables not supported outside strict)

--- test/scorecardControllerTest.js
+++ test/scorecardControllerTest.js
@@ -6,7 +6,7 @@ var courseHcp = [11,9,5,1,13,7,3,17,15];
 describe("Test ScoreCardController", function() {
     it("should get correct Vorgabe with Spielvogabe -47", function() {
 
-        let expected = [3,3,3,3,2,3,3,2,2];
+        expected = [3,3,3,3,2,3,3,2,2];
         courseHcp.forEach((value, idx) =>{
             assert.equal(controller.getVorgabe(-47,value), expected[idx]);
             // also test with positiv Spielvorgabe
@@ -16,7 +16,7 @@ describe("Test ScoreCardController", function() {
     });
     it("should get correct Vorgabe with Spielvogabe -49", function() {
 
-        let expected = [3,3,3,3,3,3,3,2,2];
+        expected = [3,3,3,3,3,3,3,2,2];
         courseHcp.forEach((value, idx) =>{
             assert.equal(controller.getVorgabe(-49,value), expected[idx]);
         })
@@ -24,7 +24,7 @@ describe("Test ScoreCardController", function() {
     });
     it ("should get correct Vorgabe with Spielvorgabe -17", () => {
         
-        let expected = [1,1,1,1,1,1,1,1,1];
+        expected = [1,1,1,1,1,1,1,1,1];
         courseHcp.forEach((value, idx) =>{
             assert.equal(controller.getVorgabe(-17,value), expected[idx]);
         })
@@ -32,7 +32,7 @@ describe("Test ScoreCardController", function() {
     });
     it ("should get correct Vorgabe with Spielvorgabe -5", () => {
         
-        let expected = [0,0,1,1,0,0,1,0,0];
+        expected = [0,0,1,1,0,0,1,0,0];
         courseHcp.forEach((value, idx) =>{
             assert.equal(controller.getVorgabe(-5,value), expected[idx]);
         })
