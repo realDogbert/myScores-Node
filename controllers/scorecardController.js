1fb8ea1326409115160cb61df8a32fdf888c5b6a
Frank von Eitzen <fveitzen@gmail.com>
2019-01-19 13:39:35 +0100

    just to test mocha ontegration in CircleCI

--- controllers/scorecardController.js
+++ controllers/scorecardController.js
@@ -1,21 +1,36 @@
+require('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+var db;
 var controller = new Object();
 
+
+MongoClient.connect(process.env.DB_CONN, 
+    { useNewUrlParser: true }, 
+    (err, client) => {
+        if (err) return console.log(err);
+        db = client.db(process.env.DB_NAME);
+    }
+);
+
 controller.getVorgabe = (spielvorgabe, hcp) => {
+
+    const numberHoles = 18;
 
     if(!spielvorgabe) spielvorgabe = -54;
     spielvorgabe = Math.abs(spielvorgabe);
 
     var vorgabe = 0;
-    if(spielvorgabe <= 18 && spielvorgabe >= hcp) {
+    if(spielvorgabe <= numberHoles && spielvorgabe >= hcp) {
         vorgabe++;
     }
-    if(spielvorgabe > 18) {
+    if(spielvorgabe > numberHoles) {
         vorgabe++;
     }
-    if(spielvorgabe -18 >= hcp) {
+    if(spielvorgabe - numberHoles >= hcp) {
         vorgabe++;
     }
-    if(spielvorgabe -36 >= hcp) {
+    if(spielvorgabe - (2*numberHoles) >= hcp) {
         vorgabe++;
     }
     return vorgabe;
