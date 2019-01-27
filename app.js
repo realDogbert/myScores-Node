ba9f9b4bcc70712de09426d2ad34ab5e6163e9d0
Frank von Eitzen <fveitzen@gmail.com>
2019-01-27 21:08:01 +0100

    session secret to environment variable

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const bcrypt = require('bcrypt');
 
 app.use(session({
-  secret: 'pleasereplacethissecret',
+  secret: process.env.SECRET_SESSION,
   resave: false,
   saveUninitialized: false,
   // cookie: { secure: true }
