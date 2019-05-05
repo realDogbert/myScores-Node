34b0698f2bad26eb05a22071a9c9d0a34c58919e
Frank von Eitzen <fveitzen@gmail.com>
2019-05-05 14:44:19 +0200

    updated npm to 10.15

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const exphbs  = require('express-handlebars');
 var cors = require('cors');
 var session = require('express-session');
 var path = require('path');
@@ -23,10 +24,16 @@ MongoClient.connect(process.env.DB_CONN,
 );
 
 // view engine setup
-var hbs = require('hbs');
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// var hbs = require('hbs');
+// hbs.registerPartials(__dirname + '/views/partials');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
