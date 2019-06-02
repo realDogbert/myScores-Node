cf01a8d8700a51afc55d9c155142772421c90fdd
Frank von Eitzen <fveitzen@gmail.com>
2019-06-02 21:46:44 +0200

    Added handicap to user model.

--- models/userModel.js
+++ models/userModel.js
@@ -8,7 +8,8 @@ const UserSchema = new mongoose.Schema(
         name: { type: String, required: true },
         email: { type: String, required: true },
         password: { type: String, required: true },
-        realName: { type: String, required: false }
+        realName: { type: String, required: false },
+        handicap: { type: Number, required: false }
     },
     { 
         timestamps: { createdAt: 'dateCreated', updatedAt: 'dateLastModified' } 
--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,13 @@ var express = require('express');
 var router = express.Router();
 var request = require("request");
 var passport = require('passport');
+var userController = require('../controllers/userController');
 
 const { check, validationResult } = require('express-validator/check');
-
-const MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID; 
-var db;
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-require('dotenv').config();
-
-MongoClient.connect(process.env.DB_CONN, 
-    { useNewUrlParser: true }, 
-    (err, client) => {
-        if (err) return console.log(err);
-        db = client.db(process.env.DB_NAME);
-    }
-);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -80,27 +68,19 @@ router.post('/register', [
     user.email = req.body.email;
     user.password = hash;  
   
-    
-    db.collection('users').insertOne(user, (err, result) => {
+    userController.create(user)
+    .then( result => {
+      req.login(result, error => {
+        res.render('user/dashboard', { 
+          title: 'Dashboard ' + user.name,
+          user: user 
+        });
+      })
+    })
+    .catch( error => {
+      res.render('register', { title: 'Error', errors: err.array() });
+    })    
 
-      if (err) return console.log(err);
-      
-      db.collection('users').findOne(  {"_id": new ObjectID(user._id)}, function(err, document) {
-
-        if(err) {
-          return res.render('register', { title: 'Error', errors: err.array() });
-        }
-        else {
-          req.login(user, function(err) {
-            return res.render('user/dashboard', { 
-              title: 'Dashboard ' + user.name,
-              user: user 
-            }); 
-          });
-        }
-      });
-
-    });
   });
 });
 
